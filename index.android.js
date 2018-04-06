949d13ff781806f5cf36ffa7688b801cc011ba7b
tangkunyin <iam@tangkunyin.com>
2018-04-06 18:42:18 +0800

    update index entry

--- index.android.js
+++
@@ -1,5 +0,0 @@
-global.__iOS__ = false;
-global.__ANDROID__ = true;
-global.__WEB__ = false;
-
-import './src';
--- index.web.js
+++
@@ -1,6 +0,0 @@
-global.__iOS__ = false;
-global.__ANDROID__ = false;
-global.__WEB__ = true;
-
-
-import './src';
