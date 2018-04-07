4808347f57fcdad3fc8858764aeaed41a42f6f56
tangkunyin <iam@tangkunyin.com>
2018-04-07 14:31:09 +0800

    IconButton size propery not works well fiexed

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -9,14 +9,14 @@ import PropTypes from 'prop-types';
 import IconType from '../constants/IconType';
 
 const IconButton = (props) => {
-    const { 
-        name, color, size, style, onPress, type, disabled 
+    const {
+        name, color, size, style, onPress, type, disabled
     } = props;
     const Icon = IconType(type);
     const iconElement = <Icon name={name} color={color || 'white'} size={size || 26} style={style} />;
     return (
         <TouchableHighlight underlayColor={'transparent'} disabled={disabled} onPress={onPress}>
-            <View style={{ width: size, height: size }}>
+            <View>
                 {iconElement}
             </View>
         </TouchableHighlight>
@@ -31,13 +31,13 @@ IconButton.propTypes = {
     onPress: PropTypes.func,
     disabled: PropTypes.bool,
     type: PropTypes.oneOf([
-        'zocial', 
-        'octicon', 
-        'material', 
-        'material-community', 
-        'ionicon', 
-        'foundation', 
-        'evilicon', 
+        'zocial',
+        'octicon',
+        'material',
+        'material-community',
+        'ionicon',
+        'foundation',
+        'evilicon',
         'entypo',
         'font-awesome',
         'simple-line-icon'
